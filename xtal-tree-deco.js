ebe13fc6b4feac76b88719e4644ec74e9d1bac3d
bahrus <anderson.bruce.b@gmail.com>
2019-08-06 20:26:51 -0400

    ...

--- xtal-tree-deco.js
+++ xtal-tree-deco.js
@@ -1,13 +1,127 @@
 import { define } from 'trans-render/define.js';
+import { decorate, } from 'trans-render/decorate.js';
 import { XtalDeco } from 'xtal-deco/xtal-deco.js';
-import { XtalTreeDecoArgs } from './XtalTreeDecoArgs.js';
-export class XtalTreeDeco extends XtalDeco {
-    constructor() {
-        super(...arguments);
-        this._decorateArgs = XtalTreeDecoArgs;
+function getStrVal(el) {
+    switch (el.localName) {
+        // case 'div':
+        //     return el.textContent;
+        case 'details':
+            return el.querySelector('summary').textContent;
+        default:
+            return el.textContent;
     }
+}
+export class XtalTreeDeco extends XtalDeco {
     static get is() {
         return 'xtal-tree-deco';
+    }
+    constructor() {
+        super();
+        this._decoratorFn = (target) => {
+            decorate(target, {
+                propDefs: {
+                    allExpanded: false,
+                    allCollapsed: false,
+                    searchString: null,
+                    sortDir: null,
+                },
+                methods: {
+                    onPropsChange(name, newVal) {
+                        const h = this;
+                        const h2 = h;
+                        switch (name) {
+                            case 'allExpanded':
+                                if (newVal) {
+                                    const t0 = performance.now();
+                                    h.setAttribute('open', '');
+                                    //const lock = this.getDisplayLock();
+                                    //this.displayLock.acquire().then(() =>{
+                                    h.querySelectorAll('details').forEach(details => details.setAttribute('open', ''));
+                                    //lock.commit();
+                                    //})
+                                    const t1 = performance.now();
+                                    console.log(t1 - t0 + ' milliseconds');
+                                }
+                                if (h2.allCollapsed)
+                                    h2.allCollapsed = false;
+                                break;
+                            case 'allCollapsed':
+                                if (newVal) {
+                                    h.removeAttribute('open');
+                                    h.querySelectorAll('details').forEach(details => details.removeAttribute('open'));
+                                }
+                                if (h2.allExpanded)
+                                    h2.allExpanded = false;
+                                break;
+                            case 'searchString':
+                                if (newVal === null || newVal === '')
+                                    return;
+                                const newValLC = newVal.toLowerCase();
+                                h.querySelectorAll('section>div').forEach(div => {
+                                    if (div.textContent.toLowerCase().indexOf(newValLC) > -1) {
+                                        div.classList.add('match');
+                                    }
+                                    else {
+                                        div.classList.remove('match');
+                                    }
+                                });
+                                const summary = h.querySelector('summary');
+                                if (summary.textContent.toLowerCase().indexOf(newValLC) > -1) {
+                                    summary.classList.add('match');
+                                    h2.allCollapsed = true;
+                                }
+                                else {
+                                    summary.classList.remove('match');
+                                    // if (recursive) {
+                                    //     this.querySelectorAll('details').forEach(details => {
+                                    //         details.searchString = newValLC;
+                                    //     });
+                                    //     if (this.querySelector('.match')) {
+                                    //         this.setAttribute('open', '');
+                                    //     }
+                                    //     else {
+                                    //         this.removeAttribute('open');
+                                    //     }
+                                    // }
+                                }
+                                break;
+                            case 'sortDir':
+                                if (newVal === null)
+                                    return;
+                                const section = this.querySelector('section');
+                                const sectionChildren = Array.from(section.children);
+                                const one = newVal === 'asc' ? 1 : -1;
+                                const min = newVal === 'asc' ? -1 : 1;
+                                sectionChildren.sort((a, b) => {
+                                    const lhs = getStrVal(a);
+                                    const rhs = getStrVal(b);
+                                    if (lhs < rhs)
+                                        return min;
+                                    if (lhs > rhs)
+                                        return one;
+                                    return 0;
+                                });
+                                let count = 1;
+                                sectionChildren.forEach(child => {
+                                    const child2 = child;
+                                    const count$ = count.toString();
+                                    child2.style.order = count$;
+                                    child2.tabIndex = parseInt(count$);
+                                    count++;
+                                });
+                                //if (recursive) {
+                                if (!h2._skipRecSort) {
+                                    h.querySelectorAll('details').forEach(details => {
+                                        details._skipRecSort = true;
+                                        details.sortDir = newVal;
+                                    });
+                                }
+                            //}
+                        }
+                    }
+                }
+            });
+        };
     }
 }
 define(XtalTreeDeco);
--- xtal-tree-deco.ts
+++ xtal-tree-deco.ts
@@ -1,10 +1,130 @@
 import {define} from 'trans-render/define.js';
+import {decorate, } from 'trans-render/decorate.js';
 import {XtalDeco} from 'xtal-deco/xtal-deco.js';
-import {XtalTreeDecoArgs} from './XtalTreeDecoArgs.js';
+import { DecorateArgs } from '../trans-render/init.d.js';
+function getStrVal(el: HTMLElement) : string {
+    switch (el.localName) {
+        // case 'div':
+        //     return el.textContent;
+        case 'details':
+            return el.querySelector('summary')!.textContent as string;
+        default:
+            return el.textContent as string;
+    }
+}
 export class XtalTreeDeco extends XtalDeco{
     static get is(){
         return 'xtal-tree-deco';
     }
-    _decorateArgs = XtalTreeDecoArgs;
+    constructor(){
+        super();
+        this._decoratorFn = (target: HTMLElement) => {
+            decorate(target, {
+                propDefs: {
+                    allExpanded: false,
+                    allCollapsed: false,
+                    searchString: null,
+                    sortDir: null,
+                },
+                methods: {
+                    onPropsChange(name: string, newVal: any) {
+                        const h = (<any>this) as HTMLElement;
+                        const h2 = h as any;
+                        switch (name) {
+                            case 'allExpanded':
+                                if (newVal) {
+                                    const t0 = performance.now();
+                                    h.setAttribute('open', '');
+                                    //const lock = this.getDisplayLock();
+                                    //this.displayLock.acquire().then(() =>{
+                                        h.querySelectorAll('details').forEach(details => details.setAttribute('open', ''));
+                                        //lock.commit();
+                                    //})
+                                    
+                                    const t1 = performance.now();
+                                    console.log(t1 - t0 + ' milliseconds');
+                                }
+                                if (h2.allCollapsed)
+                                    h2.allCollapsed = false;
+                                break;
+                            case 'allCollapsed':
+                                if (newVal) {
+                                    h.removeAttribute('open');
+                                    h.querySelectorAll('details').forEach(details => details.removeAttribute('open'));
+                                }
+                                if (h2.allExpanded)
+                                    h2.allExpanded = false;
+                                break;
+                            case 'searchString':
+                                if (newVal === null || newVal === '')
+                                    return;
+                                const newValLC = newVal.toLowerCase();
+                                h.querySelectorAll('section>div').forEach(div => {
+                                    if (div.textContent!.toLowerCase().indexOf(newValLC) > -1) {
+                                        div.classList.add('match');
+                                    }
+                                    else {
+                                        div.classList.remove('match');
+                                    }
+                                });
+                                const summary = h.querySelector('summary')!;
+                                if (summary.textContent!.toLowerCase().indexOf(newValLC) > -1) {
+                                    summary.classList.add('match');
+                                    h2.allCollapsed = true;
+                                }
+                                else {
+                                    summary.classList.remove('match');
+                                    // if (recursive) {
+                                    //     this.querySelectorAll('details').forEach(details => {
+                                    //         details.searchString = newValLC;
+                                    //     });
+                                    //     if (this.querySelector('.match')) {
+                                    //         this.setAttribute('open', '');
+                                    //     }
+                                    //     else {
+                                    //         this.removeAttribute('open');
+                                    //     }
+                                    // }
+                                }
+                                break;
+                            case 'sortDir':
+                                if (newVal === null)
+                                    return;
+                                const section = this.querySelector('section') as HTMLTableSectionElement;
+                                const sectionChildren = Array.from(section.children);
+                                const one = newVal === 'asc' ? 1 : -1;
+                                const min = newVal === 'asc' ? -1 : 1;
+                                sectionChildren.sort((a: any, b: any) => {
+                                    const lhs = getStrVal(a);
+                                    const rhs = getStrVal(b);
+                                    if (lhs < rhs)
+                                        return min;
+                                    if (lhs > rhs)
+                                        return one;
+                                    return 0;
+                                });
+                                let count = 1;
+                                sectionChildren.forEach(child => {
+                                    const child2 = child as HTMLElement;
+                                    const count$ = count.toString();
+                                    child2.style.order = count$;
+                                    child2.tabIndex = parseInt(count$);
+                                    count++;
+                                });
+                                //if (recursive) {
+                                if(!h2._skipRecSort){
+                                    h.querySelectorAll('details').forEach(details => {
+                                        (<any>details)._skipRecSort = true;
+                                        (<any>details).sortDir = newVal;
+                                    });
+                                }
+            
+                                //}
+                        }
+                    }
+                }
+            } as DecorateArgs)
+        }
+    }
 }
 define(XtalTreeDeco);
