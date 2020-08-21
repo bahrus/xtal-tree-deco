6e6daa634f3d874d26b50eb8ee65aff2ae1a8c12
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-08-21 04:20:20 -0400

    ...

--- xtal-tree-deco.js
+++ xtal-tree-deco.js
@@ -1,4 +1,3 @@
-import { decorate, } from 'trans-render/plugins/decorate.js';
 import { XtalDeco } from 'xtal-deco/xtal-deco.js';
 import { define } from 'xtal-element/xtal-latx.js';
 function getStrVal(el) {
@@ -11,109 +10,98 @@ function getStrVal(el) {
             return el.textContent;
     }
 }
+const allExpanded = 'allExpanded';
+const allCollapsed = 'allCollapsed';
+const searchString = 'searchString';
+const sortDir = 'sortDir';
+const init = ({ self }) => {
+    self.allExpanded = false;
+    self.allCollapsed = false;
+    self.searchString = null;
+    self.sortDir = null;
+};
+const actions = [
+    ({ allExpanded, self }) => {
+        if (allExpanded) {
+            const t0 = performance.now();
+            self.open = true;
+            self.querySelectorAll('details').forEach(details => details.open = true);
+            const t1 = performance.now();
+            console.log(t1 - t0 + ' milliseconds');
+        }
+        self.allCollapsed = false;
+    },
+    ({ allCollapsed, self }) => {
+        if (allCollapsed) {
+            self.removeAttribute('open');
+            self.querySelectorAll('details').forEach(details => details.removeAttribute('open'));
+        }
+        self.allExpanded = false;
+    },
+    ({ searchString, self }) => {
+        const t0 = performance.now();
+        if (searchString === null || searchString === '')
+            return;
+        self.allCollapsed = true;
+        const newValLC = searchString.toLowerCase();
+        const tNodes = Array.from(self.querySelectorAll('div, summary'));
+        tNodes.forEach(el => {
+            if (el.textContent.toLowerCase().indexOf(newValLC) > -1) {
+                el.classList.add('match');
+            }
+            else {
+                el.classList.remove('match');
+            }
+        });
+        Array.from(self.querySelectorAll('details')).forEach(detailsEl => {
+            if (detailsEl.querySelector('.match') !== null)
+                detailsEl.open = true;
+        });
+        const firstMatch = self.querySelector('.match');
+        if (firstMatch !== null) {
+            self.open = true;
+            firstMatch.scrollIntoView();
+        }
+        const t1 = performance.now();
+        console.log(t1 - t0 + ' milliseconds');
+    },
+    ({ sortDir, self }) => {
+        if (sortDir === null)
+            return;
+        const section = self.querySelector('section');
+        const sectionChildren = Array.from(section.children);
+        const one = sortDir === 'asc' ? 1 : -1;
+        const minusOne = sortDir === 'asc' ? -1 : 1;
+        sectionChildren.sort((a, b) => {
+            const lhs = getStrVal(a);
+            const rhs = getStrVal(b);
+            if (lhs < rhs)
+                return minusOne;
+            if (lhs > rhs)
+                return one;
+            return 0;
+        });
+        let count = 1;
+        sectionChildren.forEach(child => {
+            const child2 = child;
+            const count$ = count.toString();
+            child2.style.order = count$;
+            child2.tabIndex = parseInt(count$);
+            count++;
+        });
+        // if(!h2._skipRecSort){
+        //     h.querySelectorAll('details').forEach(details => {
+        //         (<any>details)._skipRecSort = true;
+        //         (<any>details).sortDir = newVal;
+        //     });
+        // }
+    }
+];
 export class XtalTreeDeco extends XtalDeco {
     constructor() {
-        super();
-        this.decoratorFn = (target) => {
-            decorate(target, {
-                propDefs: {
-                    allExpanded: false,
-                    allCollapsed: false,
-                    searchString: null,
-                    sortDir: null,
-                },
-                methods: {
-                    onPropsChange(name, newVal) {
-                        const h = this;
-                        const h2 = h;
-                        switch (name) {
-                            case 'allExpanded':
-                                if (newVal) {
-                                    const t0 = performance.now();
-                                    h.setAttribute('open', '');
-                                    //const lock = this.getDisplayLock();
-                                    //this.displayLock.acquire().then(() =>{
-                                    h.querySelectorAll('details').forEach(details => details.setAttribute('open', ''));
-                                    //lock.commit();
-                                    //})
-                                    const t1 = performance.now();
-                                    console.log(t1 - t0 + ' milliseconds');
-                                }
-                                if (h2.allCollapsed)
-                                    h2.allCollapsed = false;
-                                break;
-                            case 'allCollapsed':
-                                if (newVal) {
-                                    h.removeAttribute('open');
-                                    h.querySelectorAll('details').forEach(details => details.removeAttribute('open'));
-                                }
-                                if (h2.allExpanded)
-                                    h2.allExpanded = false;
-                                break;
-                            case 'searchString':
-                                const t0 = performance.now();
-                                if (newVal === null || newVal === '')
-                                    return;
-                                h2.allCollapsed = true;
-                                const newValLC = newVal.toLowerCase();
-                                const tNodes = Array.from(h.querySelectorAll('div, summary'));
-                                tNodes.forEach(el => {
-                                    if (el.textContent.toLowerCase().indexOf(newValLC) > -1) {
-                                        el.classList.add('match');
-                                    }
-                                    else {
-                                        el.classList.remove('match');
-                                    }
-                                });
-                                Array.from(h.querySelectorAll('details')).forEach(detailsEl => {
-                                    if (detailsEl.querySelector('.match') !== null)
-                                        detailsEl.open = true;
-                                });
-                                if (h.querySelector('.match') !== null)
-                                    h2.open = true;
-                                //if(!h.matches('.match')){
-                                h.querySelector('.match')?.scrollIntoView();
-                                //}
-                                const t1 = performance.now();
-                                console.log(t1 - t0 + ' milliseconds');
-                                break;
-                            case 'sortDir':
-                                if (newVal === null)
-                                    return;
-                                const section = this.querySelector('section');
-                                const sectionChildren = Array.from(section.children);
-                                const one = newVal === 'asc' ? 1 : -1;
-                                const min = newVal === 'asc' ? -1 : 1;
-                                sectionChildren.sort((a, b) => {
-                                    const lhs = getStrVal(a);
-                                    const rhs = getStrVal(b);
-                                    if (lhs < rhs)
-                                        return min;
-                                    if (lhs > rhs)
-                                        return one;
-                                    return 0;
-                                });
-                                let count = 1;
-                                sectionChildren.forEach(child => {
-                                    const child2 = child;
-                                    const count$ = count.toString();
-                                    child2.style.order = count$;
-                                    child2.tabIndex = parseInt(count$);
-                                    count++;
-                                });
-                                //if (recursive) {
-                                if (!h2._skipRecSort) {
-                                    h.querySelectorAll('details').forEach(details => {
-                                        details._skipRecSort = true;
-                                        details.sortDir = newVal;
-                                    });
-                                }
-                            //}
-                        }
-                    }
-                }
-            });
-        };
+        super(...arguments);
+        this.init = init; //TODO -- figure out how to de-any-fy
+        this.actions = actions;
     }
 }
 XtalTreeDeco.is = 'xtal-tree-deco';
--- xtal-tree-deco.ts
+++ xtal-tree-deco.ts
@@ -1,7 +1,7 @@
 import {decorate, } from 'trans-render/plugins/decorate.js';
 import {XtalDeco} from 'xtal-deco/xtal-deco.js';
 import { DecorateArgs } from 'trans-render/types.d.js';
-import {define} from 'xtal-element/xtal-latx.js';
+import {define, PropAction} from 'xtal-element/xtal-latx.js';
 function getStrVal(el: HTMLElement) : string {
     switch (el.localName) {
         // case 'div':
@@ -12,113 +12,106 @@ function getStrVal(el: HTMLElement) : string {
             return el.textContent as string;
     }
 }
-export class XtalTreeDeco extends XtalDeco {
-    static is =  'xtal-tree-deco';
-    constructor(){
-        super();
-        this.decoratorFn = (target: HTMLElement) => {
-            decorate(target, {
-                propDefs: {
-                    allExpanded: false,
-                    allCollapsed: false,
-                    searchString: null,
-                    sortDir: null,
-                },
-                methods: {
-                    onPropsChange(name: string, newVal: any) {
-                        const h = (<any>this) as HTMLElement;
+const allExpanded = 'allExpanded';
+const allCollapsed = 'allCollapsed';
+const searchString = 'searchString';
+const sortDir = 'sortDir';
 
-                        const h2 = h as any;
-                        switch (name) {
-                            case 'allExpanded':
-                                if (newVal) {
-                                    const t0 = performance.now();
-                                    h.setAttribute('open', '');
-                                    //const lock = this.getDisplayLock();
-                                    //this.displayLock.acquire().then(() =>{
-                                        h.querySelectorAll('details').forEach(details => details.setAttribute('open', ''));
-                                        //lock.commit();
-                                    //})
-                                    
-                                    const t1 = performance.now();
-                                    console.log(t1 - t0 + ' milliseconds');
-                                }
-                                if (h2.allCollapsed)
-                                    h2.allCollapsed = false;
-                                break;
-                            case 'allCollapsed':
-                                if (newVal) {
-                                    h.removeAttribute('open');
-                                    h.querySelectorAll('details').forEach(details => details.removeAttribute('open'));
-                                }
-                                if (h2.allExpanded)
-                                    h2.allExpanded = false;
-                                break;
-                            case 'searchString':
-                                const t0 = performance.now();
-                                if (newVal === null || newVal === '')
-                                    return;
-                                h2.allCollapsed = true;
-                                const newValLC = newVal.toLowerCase();
-                                const tNodes = Array.from(h.querySelectorAll('div, summary'));
-                                tNodes.forEach(el => {
-                                    if (el.textContent!.toLowerCase().indexOf(newValLC) > -1) {
-                                        el.classList.add('match');
-                                    }
-                                    else {
-                                        el.classList.remove('match');
-                                    }
-                                });
-                                Array.from(h.querySelectorAll('details')).forEach(detailsEl =>{
-                                    if(detailsEl.querySelector('.match') !== null) detailsEl.open = true;
-                                })
-                                if(h.querySelector('.match') !== null) h2.open = true;
-                                //if(!h.matches('.match')){
-                                    h.querySelector('.match')?.scrollIntoView();
-                                //}
-                                const t1 = performance.now();
-                                    console.log(t1 - t0 + ' milliseconds');
-                                break;
-                            case 'sortDir':
-                                if (newVal === null)
-                                    return;
-                                const section = this.querySelector('section') as HTMLTableSectionElement;
-                                const sectionChildren = Array.from(section.children);
-                                const one = newVal === 'asc' ? 1 : -1;
-                                const min = newVal === 'asc' ? -1 : 1;
-                                sectionChildren.sort((a: any, b: any) => {
-                                    const lhs = getStrVal(a);
-                                    const rhs = getStrVal(b);
-                                    if (lhs < rhs)
-                                        return min;
-                                    if (lhs > rhs)
-                                        return one;
-                                    return 0;
-                                });
-                                let count = 1;
-                                sectionChildren.forEach(child => {
-                                    const child2 = child as HTMLElement;
-                                    const count$ = count.toString();
-                                    child2.style.order = count$;
-                                    child2.tabIndex = parseInt(count$);
-                                    count++;
-                                });
-                                //if (recursive) {
-                                if(!h2._skipRecSort){
-                                    h.querySelectorAll('details').forEach(details => {
-                                        (<any>details)._skipRecSort = true;
-                                        (<any>details).sortDir = newVal;
-                                    });
-                                }
-                                
-                                //}
-                        }
-                    }
-                }
-            } as DecorateArgs)
+interface ExtendedHTMLDetailsElement extends HTMLDetailsElement{
+    allExpanded: boolean;
+    allCollapsed: boolean;
+    searchString: string | null;
+    sortDir: 'asc' | 'desc' | null;
+    self: ExtendedHTMLDetailsElement;
+}
+
+const init = ({self}: ExtendedHTMLDetailsElement) => {
+    self.allExpanded = false;
+    self.allCollapsed = false;
+    self.searchString = null;
+    self.sortDir = null;
+};
+
+const actions = [
+    ({allExpanded, self} : ExtendedHTMLDetailsElement) => {
+        if (allExpanded) {
+            const t0 = performance.now();
+            self.open = true;
+            self.querySelectorAll('details').forEach(details => details.open = true);
+            const t1 = performance.now();
+            console.log(t1 - t0 + ' milliseconds');
         }
+        self.allCollapsed = false;
+    },
+    ({allCollapsed, self}: ExtendedHTMLDetailsElement) => {
+        if (allCollapsed) {
+            self.removeAttribute('open');
+            self.querySelectorAll('details').forEach(details => details.removeAttribute('open'));
+        }
+        self.allExpanded = false;
+    },
+    ({searchString, self}: ExtendedHTMLDetailsElement) => {
+        const t0 = performance.now();
+        if (searchString === null || searchString === '')
+            return;
+        self.allCollapsed = true;
+        const newValLC = searchString.toLowerCase();
+        const tNodes = Array.from(self.querySelectorAll('div, summary'));
+        tNodes.forEach(el => {
+            if (el.textContent!.toLowerCase().indexOf(newValLC) > -1) {
+                el.classList.add('match');
+            }
+            else {
+                el.classList.remove('match');
+            }
+        });
+        Array.from(self.querySelectorAll('details')).forEach(detailsEl =>{
+            if(detailsEl.querySelector('.match') !== null) detailsEl.open = true;
+        });
+        const firstMatch = self.querySelector('.match');
+        if(firstMatch !== null){
+            self.open = true;
+            firstMatch.scrollIntoView();
+        }
+        const t1 = performance.now();
+        console.log(t1 - t0 + ' milliseconds');
+    },
+    ({sortDir, self}: ExtendedHTMLDetailsElement) => {
+        if (sortDir === null) return;
+        const section = self.querySelector('section') as HTMLTableSectionElement;
+        const sectionChildren = Array.from(section.children);
+        const one = sortDir === 'asc' ? 1 : -1;
+        const minusOne = sortDir === 'asc' ? -1 : 1;
+        sectionChildren.sort((a: any, b: any) => {
+            const lhs = getStrVal(a);
+            const rhs = getStrVal(b);
+            if (lhs < rhs)
+                return minusOne;
+            if (lhs > rhs)
+                return one;
+            return 0;
+        });
+        let count = 1;
+        sectionChildren.forEach(child => {
+            const child2 = child as HTMLElement;
+            const count$ = count.toString();
+            child2.style.order = count$;
+            child2.tabIndex = parseInt(count$);
+            count++;
+        });
+        // if(!h2._skipRecSort){
+        //     h.querySelectorAll('details').forEach(details => {
+        //         (<any>details)._skipRecSort = true;
+        //         (<any>details).sortDir = newVal;
+        //     });
+        // }
     }
+]
 
-    
+export class XtalTreeDeco<ExtendedHTMLDetailsElement> extends XtalDeco {
+    static is =  'xtal-tree-deco';
+    init = init as PropAction; //TODO -- figure out how to de-any-fy
+    actions = actions as PropAction<any>[];
 }
 define(XtalTreeDeco);
+
